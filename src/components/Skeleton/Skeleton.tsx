7c5a020acde825db73fad507a06fe50564bbc338
Milad Bayee <miladbayee@gmail.com>
2022-08-02 18:31:00 +0430

    Create Skeleton componenet

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,11 @@
+import SkeletonElement from "../SkeletonElement/SkeletonElement";
+
+const Skeleton = () => {
+  return (
+    <div>
+      <SkeletonElement type="avatar" animation="star" />
+    </div>
+  );
+};
+
+export default Skeleton;
